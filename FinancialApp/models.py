e7fe9f67a36af63f67bf5ec473d81e57ec2ee4d7
Джиоев Роберт Алексеевич <robertkryt@gmail.com>
2022-02-14 20:08:10 +0300

    Добавлены таблицы Users и Statistics

--- FinancialApp/models.py
+++ FinancialApp/models.py
@@ -4,13 +4,14 @@ from django.db import models
 
 
 # Create your models here.
-class User(models.Model):
+class Users(models.Model):
     Nickname = models.TextField(unique=True)
     Password = models.TextField()
+    Email = models.EmailField()
     Amount = models.IntegerField(default=0)
 
 
-class Statistic(models.Model):
+class Statistics(models.Model):
     UserID = models.IntegerField()
     Amount = models.IntegerField()
     Date = models.DateTimeField(default=datetime.date.today())
